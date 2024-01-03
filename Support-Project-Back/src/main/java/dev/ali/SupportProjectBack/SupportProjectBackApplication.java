3e90a20c6d70354644678a4971120517b1b6cc4c
Alicia González Castro <aliglezcastro@gmail.com>
2024-01-03 10:26:47 +0100

    created spring boot project

---
+++ Support-Project-Back/src/main/java/dev/ali/SupportProjectBack/SupportProjectBackApplication.java
@@ -0,0 +1,13 @@
+package dev.ali.SupportProjectBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportProjectBackApplication.class, args);
+	}
+
+}
---
+++ Support-Project-Back/src/test/java/dev/ali/SupportProjectBack/SupportProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ali.SupportProjectBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
