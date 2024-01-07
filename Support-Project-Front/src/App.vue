04c37f1ce26bb4449d1a37ade1d9ee18486ba5f1
Alicia González Castro <aliglezcastro@gmail.com>
2024-01-07 13:03:01 +0100

    firts login form schema

--- Support-Project-Front/src/App.vue
+++ Support-Project-Front/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="initial image"
+      class="logo"
+      src="@/assets/initialimg.png"
+      width="400"
+      height="400"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Login</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ Support-Project-Front/src/components/login/FormLogin.vue
@@ -0,0 +1,46 @@
+<script setup></script>
+
+<template>
+  <div class="FormContainer">
+    <form class="FormLogin">
+      <input
+        class="inputFormLogin"
+        type="text"
+        name="username"
+        id="username"
+        placeholder="Username"
+        v-model.trim="username"
+      />
+      <input
+        class="inputFormLogin"
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Password"
+        v-model.trim="password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+.FormContainer {
+  border: 2px #0b9d3d;
+}
+
+.FormLogin {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 3px #0b9d3d;
+}
+
+input {
+  border-radius: 5px;
+  margin: 1rem;
+}
+
+
+</style>
